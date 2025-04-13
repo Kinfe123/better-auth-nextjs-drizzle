f9d4829f304283adbd673d2c46ea3c463808633e
Kinfe123 <kinfishtech@gmail.com>
2025-04-13 13:55:32 +0300

    insert schema

--- lib/db.ts
+++ lib/db.ts
@@ -6,3 +6,4 @@ export const db = drizzle(client, { schema });
 
 export type User = typeof schema.user.$inferSelect;
 export type Session = typeof schema.session.$inferSelect;
+export type UserInsert = typeof schema.user.$inferInsert;
