ba03bdda81ca5d69c57186c05f523f6c257deaac
Kinfe123 <kinfishtech@gmail.com>
2025-04-13 03:41:01 +0300

    drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import { type Config } from "drizzle-kit";
-
+type ConfigWithoutDriver = Omit<Config, "driver">;
 export default {
   schema: "./lib/schema.ts",
   dialect: "postgresql",
   out: "./drizzle",
+  // @ts-ignore
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
-} satisfies Config;
+} satisfies ConfigWithoutDriver;
